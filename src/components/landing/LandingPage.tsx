535b0da6fd5aedce6bc9b4bbb89357ed27548935
Jeremy Yu <102616407+jeremyyuAWS@users.noreply.github.com>
2025-07-27 06:59:00 -0700

    Fix Voice Input button syntax error

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -329,8 +329,8 @@ export function LandingPage({ onStartConversation, onStartVoice }: LandingPagePr
   );
 }
 
-            <Mic className="mr-2 h-4 w-4" />
-            Voice Input
+              <Mic className="mr-2 h-4 w-4" />
+              Voice Input
           </Button>
         </div>
 
